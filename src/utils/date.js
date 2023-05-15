aab96e57d242a20fe833a3e7b99191a531e18245
liordesta <liord013@gmail.com>
2023-05-15 13:57:44 +0300

    update month date / gitignore

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 const dateFormatApiReq = (date) => {
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const month = date.getMonth().toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
   return `${year}-${month}-${day}`;
 };
